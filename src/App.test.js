3e349eb6a825e3b2f22e531e736abc2039cc9ae5
Yeramin Santana <ysantana.developer@gmail.com>
2017-05-21 10:50:09 -0400

    Rename App to Scoreboard

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './App';
-
-//TODO Finish other tests
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Application initialPlayers={[]} />, div);
-});
